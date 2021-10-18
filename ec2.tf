49b4fa8b4a12049135b42ea32969523cd5bf0b21
dev-kimdoyoung <dev-kimyodouny@github.com>
2021-10-18 16:41:35 +0900

    update ec2

--- ec2.tf
+++ ec2.tf
@@ -25,13 +25,12 @@ resource "aws_security_group" "allow_SSH" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  	}
+  }
 
-egress = [ {
-  cidr_blocks = [ "0.0.0.0:0" ]
-  description = "egress"
-  from_port = -1
-  protocol = "-1"
-  to_port = -1
-} ]
+  egress {
+    cidr_blocks = [ "0.0.0.0/0"]
+    from_port = -1
+    protocol = "-1"
+    to_port = -1
+  }
 }
